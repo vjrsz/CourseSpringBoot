fc1242063dce1b3c31647856040fc65d4f1c7d4c
vjrsz <juniornunes1008@gmail.com>
2023-10-14 10:59:15 -0300

    Initial commit

---
+++ src/test/java/com/vjrszcode/coursespringboot/CourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjrszcode.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
