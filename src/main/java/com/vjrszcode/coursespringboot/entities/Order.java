6b79e27d4a9d545ced55650d36a91342acb61528
vjrsz <juniornunes1008@gmail.com>
2023-10-15 10:12:34 -0300

    Add OrderStatus

--- src/main/java/com/vjrszcode/coursespringboot/entities/Order.java
+++ src/main/java/com/vjrszcode/coursespringboot/entities/Order.java
@@ -2,6 +2,7 @@ package com.vjrszcode.coursespringboot.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vjrszcode.coursespringboot.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -23,10 +25,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -43,6 +46,14 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
@@ -66,11 +77,14 @@ public class Order implements Serializable {
         return Objects.hash(id);
     }
 
+    ;
+
     @Override
     public String toString() {
         return "Order{" +
                 "id=" + id +
                 ", moment=" + moment +
+                ", orderStatus=" + orderStatus +
                 ", client=" + client +
                 '}';
     }
